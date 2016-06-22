642e28c613d229718f37c6537b6368b66de70d0c
Tyson Gach <tyson@thoughtbot.com>
2016-06-21 22:36:08 -0400

    Use SVG for the logo

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -7,7 +7,7 @@ module ApplicationHelpers
 
   def svg(name)
     root = Middleman::Application.root
-    file_path = "#{root}/source/images/#{name}.svg"
+    file_path = "#{root}/source/assets/images/#{name}.svg"
     return File.read(file_path) if File.exists?(file_path)
     "(not found)"
   end
